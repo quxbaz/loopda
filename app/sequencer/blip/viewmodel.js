770b242cc0de34f325e78cdf47d411d90a71f1b1
David Yeung <quxbaz@gmail.com>
2016-01-14 21:51:27 -0500

    Added method toPercent to blip/viewmodel

--- app/sequencer/blip/viewmodel.js
+++ app/sequencer/blip/viewmodel.js
@@ -2,10 +2,19 @@
   Blip View-Model
 */
 
+import {titleCase} from 'util';
+
 export default class {
 
   constructor(blip) {
     this.model = blip;
+  }
+
+  toPercent(prop) {
+    let value = this.model.state[prop];
+    let min = this.model.state['min' + titleCase(prop)];
+    let max = this.model.state['max' + titleCase(prop)];
+    return Math.round((value - min) / (max - min) * 100);
   }
 
 };
