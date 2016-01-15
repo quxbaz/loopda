f1036d2e0a3d3af86ecffd765fd7aece66ba5ca4
David Yeung <quxbaz@gmail.com>
2016-01-14 19:47:24 -0500

    Added util function fireOnce()

--- app/util.js
+++ app/util.js
@@ -13,3 +13,13 @@ export function requireProps(object, props) {
 export function constrain(val, minmax) {
   return Math.min(Math.max(val, minmax[0]), minmax[1]);
 }
+
+export function fireOnce(el, event, handler) {
+  /*
+    Fires a handler in response to an event, then detaches it.
+  */
+  el.addEventListener(event, function once() {
+    handler.apply(null, arguments);
+    el.removeEventListener(event, once);
+  });
+}
