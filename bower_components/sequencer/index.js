4474ee801b0ed3fb4be7038f16f2297002f3e230
David Yeung <quxbaz@gmail.com>
2016-01-18 03:57:18 -0500

    Updated sequencer

--- bower_components/sequencer/index.js
+++ bower_components/sequencer/index.js
@@ -2,6 +2,4 @@ import Sequencer from './lib/sequencer';
 import Channel from './lib/channel';
 import Blip from './lib/blip';
 import AudioService from './lib/audioservice';
-
 export {Sequencer, Channel, Blip, AudioService};
-export default {Sequencer, Channel, Blip, AudioService};
