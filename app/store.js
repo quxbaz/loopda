6fb632f32dd177f6d2055b7967177ec4df9b9608
David Yeung <quxbaz@gmail.com>
2016-02-07 20:49:42 -0500

    Now extending store.js as MappedStore

--- app/store.js
+++ app/store.js
@@ -2,14 +2,32 @@ import Store from 'store';
 import LSAdapter from 'store/lib/adapters/ls/adapter';
 import {initModels} from './models';
 
-let store = new Store({
-  adapter: new LSAdapter()
-});
+class MappedStore extends Store {
 
+  constructor(...args) {
+    super(...args);
+    this._map = new WeakMap();
+  }
+
+  map(object, record) {
+    // Maps an object to a record
+    this._map.set(object, record);
+  }
+
+  recordFor(object) {
+    return this._map.get(object);
+  }
+
+  createRecord(modelName, state, objectToMap) {
+    let record = super.createRecord(modelName, state);
+    if (objectToMap !== undefined)
+      this.map(objectToMap, record);
+    return record;
+  }
+
+}
+
+let store = new MappedStore({adapter: new LSAdapter()});
 initModels(store);
-
-let map = new WeakMap();
-store.map = (model, record) => map.set(model, record);
-store.recordFor = (model) => map.get(model);
 
 export default store;
