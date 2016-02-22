cd6201decec154efc63fa17a55ce7ea44ac5ca9d
David Yeung <quxbaz@gmail.com>
2016-02-22 01:46:54 -0500

    ChannelWrapper component now stateless

--- app/components/overview/channel-wrapper.jsx
+++ app/components/overview/channel-wrapper.jsx
@@ -1,58 +1,44 @@
 import React from 'react';
 import classNames from 'classnames';
+import store from 'globals/store';
+import sequencerCtrl from 'controllers/sequencer/sequencer';
 import channelCtrl from 'controllers/sequencer/channel';
 import blipCtrl from 'controllers/sequencer/blip';
 import ChannelComponent from 'components/sequencer/channel';
-import store from 'globals/store';
 
-export default React.createClass({
+ChannelWrapper.propTypes = {
+  channel: React.PropTypes.object.isRequired,
+  currentBeat: React.PropTypes.number.isRequired,
+  onRemove: React.PropTypes.func
+};
 
-  propTypes: {
-    channel: React.PropTypes.object.isRequired,
-    currentBeat: React.PropTypes.number.isRequired,
-  },
-
-  remove() {
-    this.props.onRemove(this.props.channel);
-  },
-
-  viewChannelDetail() {
-    channelCtrl.viewChannelDetail(this.props.channel);
-  },
-
-  toggleMute() {
-    channelCtrl.toggleMute(this.props.channel);
-  },
-
-  render() {
-    let {channel} = this.props;
-    let record = store.recordFor(channel);
-    let classes = classNames({
-      channel: true,
-      mute: channel.state.mute
-    });
-    let channelProps = {
-      channel,
-      currentBeat: this.props.currentBeat,
-      onClickBlip: blipCtrl.toggleMute
-    };
-    return (
-      <div className={classes}>
-        <div className="channel-title">
-          <div>
-            <a onClick={this.viewChannelDetail}>{record.state.title}</a>
-          </div>
-          {channel.state.sampleName} -
-          (<a onClick={this.remove}>remove</a>) -
-          (<a onClick={this.toggleMute}>
-            {channel.state.mute ? 'unmute' : 'mute'}
-          </a>)
+export default function ChannelWrapper(props) {
+  let {channel} = props;
+  let record = store.recordFor(channel);
+  // Event handlers
+  let remove = props.onRemove.bind(null, channel);
+  let viewChannelDetail = channelCtrl.viewChannelDetail.bind(null, channel);
+  let toggleMute = channelCtrl.toggleMute.bind(null, channel);
+  let classes = classNames({
+    channel: true,
+    mute: channel.state.mute
+  });
+  return (
+    <div className={classes}>
+      <div className="channel-title">
+        <div>
+          <a onClick={viewChannelDetail}>{record.state.title}</a>
         </div>
-        <div className="inner-channel">
-          <ChannelComponent {...channelProps} />
-        </div>
+        {channel.state.sampleName} -
+        (<a onClick={remove}>remove</a>) -
+        (<a onClick={toggleMute}>
+          {channel.state.mute ? 'unmute' : 'mute'}
+        </a>)
       </div>
-    );
-  }
-
-});
+      <div className="inner-channel">
+        <ChannelComponent channel={channel} currentBeat={props.currentBeat}
+                          onClickBlip={blipCtrl.toggleMute} />
+      </div>
+    </div>
+  );
+};
