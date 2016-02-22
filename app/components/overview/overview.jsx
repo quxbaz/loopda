394d2f6218726dc1d58522d8682b00c5f88bbf41
David Yeung <quxbaz@gmail.com>
2016-02-22 01:56:56 -0500

    Overview now stateless

--- app/components/overview/overview.jsx
+++ app/components/overview/overview.jsx
@@ -7,46 +7,38 @@ import sequencerCtrl from 'controllers/sequencer/sequencer';
 import ChannelGridComponent from './channel-grid';
 import SampleOptionsComponent from './sample-options';
 
-export default React.createClass({
+Overview.propTypes = {
+  sequencer: React.PropTypes.object
+};
 
-  propTypes: {
-    sequencer: React.PropTypes.object
-  },
+export default function Overview(props) {
 
-  togglePlay() {
-    sequencerCtrl.togglePlay(this.props.sequencer);
-  },
+  let {sequencer} = props;
+  let {playing, channels, currentBeat} = sequencer.state;
 
-  addChannel(sampleName) {
-    sequencerCtrl.createChannel(this.props.sequencer, sampleName);
-  },
+  let togglePlay = sequencerCtrl.togglePlay.bind(null, sequencer);
+  let addChannel = sequencerCtrl.createChannel.bind(null, sequencer);
+  let removeChannel = sequencerCtrl.removeChannel.bind(null, sequencer);
 
-  removeChannel(channel) {
-    sequencerCtrl.removeChannel(this.props.sequencer, channel);
-  },
-
-  render() {
-    let {sequencer} = this.props;
-    let {channels, currentBeat} = sequencer.state;
-    return (
-      <div className="sequencer">
-        <a className="togglePlay" onClick={this.togglePlay}>
-          {this.props.sequencer.state.playing ? 'pause' : 'play'}
-        </a>
-        <hr />
-        <div className="channels">
-          <ChannelGridComponent channels={channels} currentBeat={currentBeat} onRemove={this.removeChannel} />
-          <div className="add-channel">
-            <div>&nbsp;</div>
-            <div className="inner">
-              <h4>Add a channel</h4>
-              <SampleOptionsComponent onClickOption={this.addChannel} />
-            </div>
+  return (
+    <div className="sequencer">
+      <a className="togglePlay" onClick={togglePlay}>
+        {playing ? 'pause' : 'play'}
+      </a>
+      <hr />
+      <div className="channels">
+        <ChannelGridComponent channels={channels} currentBeat={currentBeat}
+                              onRemove={removeChannel} />
+        <div className="add-channel">
+          <div>&nbsp;</div>
+          <div className="inner">
+            <h4>Add a channel</h4>
+            <SampleOptionsComponent onClickOption={addChannel} />
           </div>
-          <div className="spacer"></div>
         </div>
+        <div className="spacer"></div>
       </div>
-    );
-  }
+    </div>
+  );
 
-});
+};
