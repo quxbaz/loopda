36d0eaed558dea2cce741cd9df4605dcae3a695c
David Yeung <quxbaz@gmail.com>
2016-02-23 00:29:40 -0500

    BlipCtrl: setBlipState, playBlip

--- app/controllers/sequencer/blip.js
+++ app/controllers/sequencer/blip.js
@@ -2,6 +2,14 @@ import {toggleState} from 'lib/util';
 
 export default {
 
+  playBlip(blip) {
+    blip.play();
+  },
+
+  setBlipState(blip, state) {
+    blip.setState(state);
+  },
+
   toggleMute(blip) {
     toggleState(blip, 'mute');
   }
