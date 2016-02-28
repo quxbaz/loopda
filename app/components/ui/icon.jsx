85d05956a6240b7c78539f939fa6d08a05c287bb
David Yeung <quxbaz@gmail.com>
2016-02-28 02:46:12 -0500

    Icon now accepts className instead of classes

--- app/components/ui/icon.jsx
+++ app/components/ui/icon.jsx
@@ -3,20 +3,20 @@ import classNames from 'classnames';
 
 Icon.propTypes = {
   name: React.PropTypes.string.isRequired,
-  classes: React.PropTypes.object,
+  className: React.PropTypes.string,
   onClick: React.PropTypes.func
 };
 
 Icon.defaultProps = {
-  classes: {}
+  className: ''
 };
 
 export default function Icon(props) {
-  let {name, classes, onClick} = props;
-  let className = classNames(Object.assign({
+  let {name, onClick} = props;
+  let className = classNames({
     icon: true,
     clicky: props.onClick
-  }, classes));
+  }) + ' ' + props.className;
   // JSX does not support xlink:href, so we need raw HTML.
   return (
     <span className={className} onClick={onClick}>
