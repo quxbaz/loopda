caab4891606209f58c83e5a2c7a62591262e92ed
David Yeung <quxbaz@gmail.com>
2016-02-29 21:55:44 -0500

    Reordering

--- app/controllers/sequencer/channel.js
+++ app/controllers/sequencer/channel.js
@@ -4,14 +4,14 @@ import {toggleState} from 'lib/util';
 
 export default {
 
-  toggleMute(channel) {
-    toggleState(channel, 'mute');
-  },
-
   viewChannelDetail(channel) {
     let record = store.recordFor(channel);
     let id = record.state.id || record.cid;
     router.nav('/sequencer/channel/' + id);
+  },
+
+  toggleMute(channel) {
+    toggleState(channel, 'mute');
   },
 
   toggleSolo(channel) {
