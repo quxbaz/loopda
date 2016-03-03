91a9e48f6f4041a17d9d966339ed1e7f7e948427
David Yeung <quxbaz@gmail.com>
2016-03-03 00:15:31 -0500

    Gutted out mixer functionality to prepare it to work w/ blips and presets

--- app/components/mixer/mixer.jsx
+++ app/components/mixer/mixer.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {capitalize, throttle} from 'lib/util';
-import BlipCtrl from 'controllers/sequencer/blip';
+import {capitalize/*, throttle*/} from 'lib/util';
+// import BlipCtrl from 'controllers/sequencer/blip';
 import Slider from 'components/ui/slider';
 
-let playBlip = throttle((blip) => BlipCtrl.playBlip(blip), 70);
+// let playBlip = throttle((blip) => BlipCtrl.playBlip(blip), 70);
 
 export default React.createClass({
 
   propTypes: {
-    blip: React.PropTypes.object.isRequired
+    mixable: React.PropTypes.object.isRequired
   },
 
   setBlipState(prop, value) {
-    BlipCtrl.setBlipState(this.props.blip, {
-      [prop]: value
-    });
-    playBlip(this.props.blip);
+    // BlipCtrl.setBlipState(this.props.blip, {
+    //   [prop]: value
+    // });
+    // playBlip(this.props.blip);
   },
 
   render() {
 
-    let {state} = this.props.blip;
+    let {state} = this.props.mixable;
     let partial = (prop) => (value) => this.setBlipState(prop, value);
 
     // <TEMP> Get this from some other source.
-    let mixerProps = ['offset', 'gain', 'rate'];
+    let mixerProps = ['gain', 'rate'];
 
     let sliders = mixerProps.map((prop, i) => {
       return React.createElement(Slider, {
