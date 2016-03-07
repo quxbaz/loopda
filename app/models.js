96c094b5898b9ad9fca53bbc98baa0f88c786136
David Yeung <quxbaz@gmail.com>
2016-03-07 15:09:40 -0500

    Using store.define

--- app/models.js
+++ app/models.js
@@ -8,62 +8,69 @@ export let initModels = (store) => {
     return;
   initialized = true;
 
-  store.registerModel('sequencer', '/sequencer/', {
-    id: attr(),
-    playing: attr(),
-    currentBeat: attr(),
-    beats: attr(),
-    beatDuration: attr(),
-    channels: hasMany('channel')
+  store.define('sequencer', {
+    schema: {
+      id: attr(),
+      playing: attr(),
+      currentBeat: attr(),
+      beats: attr(),
+      beatDuration: attr(),
+      channels: hasMany('channel')
+    }
   });
 
-  store.registerModel('channel', '/channel/', {
-    id: attr(),
-    number: attr(),
-    title: attr(),
-    beats: attr(),
-    solo: attr(),
-    mute: attr(),
-    sample: attr(),
-    color: attr(),
-    time_created: attr(),
-    archived: attr(),
-    sequencer: belongsTo('sequencer'),
-    preset: belongsTo('preset'),
-    blips: hasMany('blip'),
+  store.define('channel', {
+    schema: {
+      id: attr(),
+      number: attr(),
+      title: attr(),
+      beats: attr(),
+      solo: attr(),
+      mute: attr(),
+      sample: attr(),
+      color: attr(),
+      time_created: attr(),
+      archived: attr(),
+      sequencer: belongsTo('sequencer'),
+      preset: belongsTo('preset'),
+      blips: hasMany('blip')
+    }
   });
 
-  store.registerModel('blip', '/blip/', {
-    id: attr(),
-    beat: attr(),
-    sample: attr(),
-    mute: attr(),
-    duration: attr(),
-    offset: attr(),
-    gain: attr(),
-    rate: attr(),
-    unmixed: attr(),
-    channel: belongsTo('channel')
+  store.define('blip', {
+    schema: {
+      id: attr(),
+      beat: attr(),
+      sample: attr(),
+      mute: attr(),
+      duration: attr(),
+      offset: attr(),
+      gain: attr(),
+      rate: attr(),
+      unmixed: attr(),
+      channel: belongsTo('channel')
+    }
   });
 
-  store.registerModel('preset', '/preset/', {
-    id: attr(),
-    title: attr(),
-    sample: attr(),
-    channels: hasMany('channel'),
+  store.define('preset', {
+    schema: {
+      id: attr(),
+      title: attr(),
+      sample: attr(),
+      channels: hasMany('channel'),
 
-    /*
-      mixable props
-    */
+      /*
+        mixable props
+      */
 
-    gain: attr(1),
-    minGain: attr(0),
-    maxGain: attr(10),
+      gain: attr(1),
+      minGain: attr(0),
+      maxGain: attr(10),
 
-    rate: attr(1),
-    minRate: attr(0),
-    maxRate: attr(4)
-
+      rate: attr(1),
+      minRate: attr(0),
+      maxRate: attr(4)
+    }
   });
 
 };
