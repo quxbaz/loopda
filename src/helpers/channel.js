dff17efa97dbc38e957f3dcc98190d6c81cf41cb
David Yeung <quxbaz@gmail.com>
2016-04-20 12:23:21 -0400

    Moved random channel color function to trax/util

--- src/helpers/channel.js
+++ src/helpers/channel.js
@@ -6,14 +6,6 @@ export default {
       let diff = a.state.time_created - b.state.time_created;
       return diff / Math.abs(diff);
     });
-  },
-
-  randomChannelHSL() {
-    /*
-      Generates a random color (within a limited palette) for a channel
-    */
-    let hue = Math.random() * 160 + 200;
-    return `hsl(${hue}, 100%, 70%)`;
   }
 
 };
