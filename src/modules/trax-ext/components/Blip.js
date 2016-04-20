0b434d6f99cf069e5a86a86588a915773ea511c9
David Yeung <quxbaz@gmail.com>
2016-04-20 16:01:17 -0400

    Blips toggleable

--- src/modules/trax-ext/components/Blip.js
+++ src/modules/trax-ext/components/Blip.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import classNames from 'classnames'
 
-const Blip = ({blip}) => {
-  return (
-    <div className="blip">[{blip.beat}]</div>
-  )
+const Blip = ({blip, onClick}) => {
+
+  let {id, mute, color} = blip
+
+  const cssClass = classNames({
+    blip: true,
+    clicky: true,
+    enabled: !mute,
+  })
+
+  const handleClick = () => onClick(id)
+
+  return <div className={cssClass} style={{background: !mute && color}}
+              onMouseDown={handleClick} />
+
 }
 
 Blip.propTypes = {
-  blip: React.PropTypes.object.isRequired
+  blip: React.PropTypes.object.isRequired,
+  onClick: React.PropTypes.func
 }
 
 export default Blip
--- src/modules/trax-ext/containers/Blip.js
+++ src/modules/trax-ext/containers/Blip.js
@@ -6,6 +6,13 @@ const mapStateToProps = (state, {id}) => ({
   blip: blips.selectors.getById(id)(state)
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  onClick: (id) => {
+    dispatch(blips.actions.toggleMuteBlip(id))
+  }
+})
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Blip)
