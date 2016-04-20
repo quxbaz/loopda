e8570cf32dbfca21493e879e263b2ec6ff3f04f2
David Yeung <quxbaz@gmail.com>
2016-04-20 02:26:49 -0400

    Added AddChannel file

---
+++ src/modules/overview/containers/AddChannel.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {channels} from 'trax'
+
+const AddChannel = ({onSelect}) => (
+  <div>
+    <strong>Add a channel</strong>
+    <ul>
+      <li><a onClick={() => onSelect('hihat')}>hihat</a></li>
+      <li><a onClick={() => onSelect('snare')}>snare</a></li>
+      <li><a onClick={() => onSelect('kick')}>kick</a></li>
+      <li><a onClick={() => onSelect('clap')}>clap</a></li>
+    </ul>
+  </div>
+)
+
+AddChannel.propTypes = {
+  onSelect: React.PropTypes.func.isRequired
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onSelect: (sample) => {
+    dispatch(channels.actions.createChannel({
+      sample,
+      title: sample,
+      number: 1
+    }))
+  }
+})
+
+export default connect(
+  null, mapDispatchToProps
+)(AddChannel)
