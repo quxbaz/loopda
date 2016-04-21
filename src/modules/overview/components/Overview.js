a8314cd024547f339433478c8708f01ba0e4cac2
David Yeung <quxbaz@gmail.com>
2016-04-21 13:34:01 -0400

    Added console.clear test button

--- src/modules/overview/components/Overview.js
+++ src/modules/overview/components/Overview.js
@@ -16,9 +16,10 @@ const Overview = ({sequencer}) => {
       {/*<EscapeWatcher onEscape={navSongMode} />*/}
       {/*<div><a href="/#/preset">presets</a></div>*/}
       {/*<div><a onClick={navSongMode}>song mode (esc)</a></div>*/}
-      <div>
-        <a onClick={() => localStorage.clear()}>localStorage.clear()</a>
-      </div>
+      <ul>
+        <li><a onClick={() => console.clear()}>console.clear()</a></li>
+        <li><a onClick={() => localStorage.clear()}>localStorage.clear()</a></li>
+      </ul>
       {/*<a className="togglePlay" onClick={togglePlay}>
         {playing ? 'pause' : 'play'} (space)
       </a>*/}
