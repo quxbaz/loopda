c7c655c86363431efaad52827dc918adece8615f
David Yeung <quxbaz@gmail.com>
2016-04-22 00:01:15 -0400

    Style

--- src/perf/index.js
+++ src/perf/index.js
@@ -1,16 +1,14 @@
 import ReactPerf from 'react/lib/ReactDefaultPerf'
 
-const time = (fn) => {
+export const time = (fn) => {
   console.time('test/time')
   fn()
   console.timeEnd('test/time')
 }
 
-const reactProfile = (fn, print=true) => {
+export const reactProfile = (fn, print=true) => {
   ReactPerf.start(); fn(); ReactPerf.stop()
   ReactPerf.printWasted(
     ReactPerf.getLastMeasurements()
   )
 }
-
-export {time, reactProfile}
