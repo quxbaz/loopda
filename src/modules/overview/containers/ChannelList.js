cfaf6350108743f2150f9a9e74e6f508ede055c7
David Yeung <quxbaz@gmail.com>
2016-04-22 19:35:01 -0400

    ChannelList now pure

--- src/modules/overview/containers/ChannelList.js
+++ src/modules/overview/containers/ChannelList.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {channels} from 'trax'
+import {PureComponent} from 'loopda/lib/react-ext'
 import ChannelBlock from './ChannelBlock'
 import ArchivedChannel from './ArchivedChannel'
 
-const ChannelList = ({channels}) => (
-  <div className="channel-list">
-    {channels.map(channel => {
-      if (channel.archived)
-        return <ArchivedChannel key={channel.id} id={channel.id} />
-      return <ChannelBlock key={channel.id} id={channel.id} />
-    })}
-  </div>
-)
+class ChannelList extends PureComponent {
+  render() {
+    const {channels} = this.props
+    return (
+      <div className="channel-list">
+        {channels.map(channel => {
+          if (channel.archived)
+            return <ArchivedChannel key={channel.id} id={channel.id} />
+          return <ChannelBlock key={channel.id} id={channel.id} />
+        })}
+      </div>
+    )
+  }
+}
 
 ChannelList.propTypes = {
   channels: React.PropTypes.array.isRequired
 }
 
+// <TODO> Fix: This query creates a new array and breaks identity comparison
 const mapStateToProps = (state) => ({
   channels: channels.selectors.getAll(state)
 })
