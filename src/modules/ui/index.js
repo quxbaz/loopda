f0ce20eeccaef7459813f4c282edf2be9a89d3ad
David Yeung <quxbaz@gmail.com>
2016-04-24 13:49:02 -0400

    Selectors import/export

--- src/modules/ui/index.js
+++ src/modules/ui/index.js
@@ -1,6 +1,7 @@
 import actionTypes from './actions'
 import actions from './actions'
 import reducer from './reducer'
+import selectors from './selectors'
 import components from './components'
 
-export default {actionTypes, actions, reducer, components}
+export default {actionTypes, actions, reducer, selectors, components}
