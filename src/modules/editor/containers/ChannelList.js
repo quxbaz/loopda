7b4716dcbfc909b3b66b7888bccc824a5d3cac3c
David Yeung <quxbaz@gmail.com>
2016-05-04 02:12:04 -0400

    Beginning channel list stuff

--- src/modules/editor/containers/ChannelList.js
+++ src/modules/editor/containers/ChannelList.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {channels} from 'trax'
+import traxExt from '../../trax-ext'
 
-const ChannelList = () => (
-  <div>ChannelList</div>
+const ChannelList = ({channels, onClickEmpty, onClickChannel}) => (
+  <div className="editor-channel-list">
+    <a onClick={onClickEmpty}>Empty</a>
+    {channels.map(channel =>
+      <traxExt.components.Channel key={channel.id} channel={channel}
+       onClick={onClickChannel} />
+    )}
+  </div>
 )
 
-export default ChannelList
+ChannelList.propTypes = {
+  channels: React.PropTypes.array.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  channels: channels.selectors.getAll(state),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  onClickEmpty: () => {
+    console.log('click empty')
+  },
+  onClickChannel: (id) => {
+    console.log('click channel')
+  },
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ChannelList)
