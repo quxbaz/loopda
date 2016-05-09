a32ac4c86a3f67d53307665f21c8e9afdba5ef29
David Yeung <quxbaz@gmail.com>
2016-05-08 23:45:37 -0400

    Formatting

--- src/globals/samples.js
+++ src/globals/samples.js
@@ -1,9 +1,6 @@
-const map = {
+export default {
   "hihat" : "media/samples/hihat.mp3",
   "clap"  : "media/samples/clap.mp3",
   "kick"  : "media/samples/kick.mp3",
-  "snare" : "media/samples/snare.mp3"
+  "snare" : "media/samples/snare.mp3",
 }
-
-export default map
-export const names = Object.keys(map)
