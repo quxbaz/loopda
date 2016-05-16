57fd9fe086fbc8abbdc3b04f168078db7f471f10
David Yeung <quxbaz@gmail.com>
2016-05-16 01:49:23 -0400

    comma

--- src/modules/trax-ext/containers/Blip.js
+++ src/modules/trax-ext/containers/Blip.js
@@ -3,7 +3,7 @@ import {blips} from 'trax'
 import Blip from '../components/Blip'
 
 const mapStateToProps = (state, {id}) => ({
-  blip: blips.selectors.getById(id)(state)
+  blip: blips.selectors.getById(id)(state),
 })
 
 export default connect(
