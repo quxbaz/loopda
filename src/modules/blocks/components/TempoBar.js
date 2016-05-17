84b06f5a46a6b5323d3ba8659c14a67ccb16b656
David Yeung <quxbaz@gmail.com>
2016-05-16 22:35:51 -0400

    Formatting

--- src/modules/blocks/components/TempoBar.js
+++ src/modules/blocks/components/TempoBar.js
@@ -4,9 +4,8 @@ import {PureComponent} from 'loopda/lib/react-ext'
 class TempoBar extends PureComponent {
 
   render() {
-    const {beat} = this.props
     const style = {
-      left: 100 / 16 * beat + '%',
+      left: 100 / 16 * this.props.beat + '%',
     }
     return <div className="tempo-bar" style={style} />
   }
