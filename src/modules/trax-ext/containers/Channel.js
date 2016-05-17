76ba223785272751190fbc25563d4b8a5b58322d
David Yeung <quxbaz@gmail.com>
2016-05-17 00:17:53 -0400

    Moved getBeatClicked to util

--- src/modules/trax-ext/containers/Channel.js
+++ src/modules/trax-ext/containers/Channel.js
@@ -1,21 +1,7 @@
 import {connect} from 'react-redux'
 import Channel from '../components/Channel'
 import {channels} from 'trax'
-
-const BEATS = 16
-
-const getBeatClicked = (event, el) => {
-  /*
-    Detects which beat position was clicked by looking at the position
-    of the mouse click compared to the dimensions of the channel
-    element. This exists as an optimization that bypasses the need to
-    render individual click-watching blip elements.
-  */
-  const {left, right} = el.getBoundingClientRect()
-  const channelWidth = right - left
-  const blipWidth = channelWidth / BEATS
-  return Math.floor((event.clientX - left) / blipWidth)
-}
+import util from '../util'
 
 const mapStateToProps = (state, {id}) => ({
   channel: channels.selectors.getById(id)(state),
@@ -23,14 +9,14 @@ const mapStateToProps = (state, {id}) => ({
 
 const mapDispatchToProps = (dispatch, {id}) => ({
   onMouseDown: (event, el) => {
-    const beat = getBeatClicked(event, el)
+    const beat = util.getBeatClicked(event, el)
     dispatch(channels.actions.toggleBlipAt(id, beat))
     return dispatch((dispatch, getState) => {
       return channels.selectors.getBlipAt(id, beat)(getState()).mute
     })
   },
   onMouseMove: (event, el, blipWasMuted) => {
-    const beat = getBeatClicked(event, el)
+    const beat = util.getBeatClicked(event, el)
     if (blipWasMuted) {
       dispatch(channels.actions.muteBlipAt(id, beat))
     } else {
