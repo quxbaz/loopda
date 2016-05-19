cb1a802dfd9020ab793256dc367f79c7b9f33fa3
David Yeung <quxbaz@gmail.com>
2016-05-19 02:28:27 -0400

    Fixed two instances of sequential ids breaking app

--- src/modules/dashboard/actions.js
+++ src/modules/dashboard/actions.js
@@ -5,14 +5,11 @@ const createDefaultSong = (title) => (dispatch) => {
   /*
     Creates a song with a new block and sets the url to that song.
   */
-
-  const blockAction = blocks.actions.createBlock()
-  dispatch(blockAction)
-  const songAction = songs.actions.createSong({
+  const blockAction = dispatch(blocks.actions.createBlock())
+  const songAction = dispatch(songs.actions.createSong({
     title: title.trim(),
     blocks: [blockAction.payload.id],
-  })
-  dispatch(songAction)
+  }))
   dispatch(url.actions.setBrowserUrl('/songs/' + songAction.payload.id))
 }
 
--- src/modules/trax-ext/actions.js
+++ src/modules/trax-ext/actions.js
@@ -4,14 +4,12 @@ import util from './util'
 const createChannel = (state) => (dispatch, getState) => {
   const storeState = getState()
   const all = channels.selectors.getAll(storeState)
-  const action = channels.actions.createChannel({
+  return dispatch(channels.actions.createChannel({
     number: util.getNextNumber(all),
     title: presets.selectors.getById(state.preset)(storeState).sample,
     color: util.randomChannelHSL(),
     ...state,
-  })
-  dispatch(action)
-  return action
+  }))
 }
 
 export default {createChannel}
