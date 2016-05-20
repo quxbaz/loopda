320bbc7e03ae8cc4e000e942191a66757b9142a9
David Yeung <quxbaz@gmail.com>
2016-05-20 15:05:06 -0400

    Added BlockControls basic component and position label in NavPane

--- src/modules/blocks/components/NavPaneBlock.js
+++ src/modules/blocks/components/NavPaneBlock.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import traxExt from '../../trax-ext'
+import NavPaneBlockControls from '../containers/NavPaneBlockControls'
 
 const NavPaneBlock = (props) => (
   <div className="nav-pane-block">
+    <div className="block-position-label">{props.i + 1}</div>
     <traxExt.components.Block {...props} />
+    <NavPaneBlockControls id={props.block.id} />
   </div>
 )
 
