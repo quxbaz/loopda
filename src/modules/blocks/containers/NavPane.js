9d335dac6c7cd9ae12d23b3b3b20247c406347a6
David Yeung <quxbaz@gmail.com>
2016-05-19 21:18:34 -0400

    Nav pane can now be mouse dragged

--- src/modules/blocks/containers/NavPane.js
+++ src/modules/blocks/containers/NavPane.js
@@ -1,16 +1,55 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fireOnce} from 'dom-util'
 import {blocks} from 'trax'
 import traxExt from '../../trax-ext'
 import url from '../../url'
 
 class NavPane extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseMove = this.handleMouseMove.bind(this)
+    this.state = {
+      isMouseDown: false,
+      isDragging: false,
+      originX: undefined,
+      scrollX: undefined,
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(window, 'mouseup', this.onMouseUp)
+  }
+
+  handleMouseDown(event) {
+    this.setState({
+      isMouseDown: true,
+      isDragging: false,
+      originX: event.clientX,
+      scrollX: this.refs.el.scrollLeft,
+    })
+    this.onMouseUp = fireOnce(window, 'mouseup', (event) => {
+      this.setState({isMouseDown: false})
+    })
+  }
+
+  handleMouseMove(event) {
+    if (this.state.isMouseDown) {
+      const diff = this.state.originX - event.clientX
+      if (Math.abs(diff) > 20)
+        this.setState({isDragging: true})
+      this.refs.el.scrollLeft = this.state.scrollX + diff
+    }
+  }
+
   render() {
     const {blocks, onClickBlock} = this.props
     return (
-      <div className="block-nav-pane">
-        <traxExt.components.BlockList blocks={blocks} onClickBlock={onClickBlock} />
+      <div ref="el" className="block-nav-pane" onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove}>
+        <traxExt.components.BlockList blocks={blocks}
+          onClickBlock={this.state.isDragging ? undefined : onClickBlock} />
       </div>
     )
   }
