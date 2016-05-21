0edd3562021ba94d0eb2c4eaa7ee0b142fa65517
David Yeung <quxbaz@gmail.com>
2016-05-21 14:25:08 -0400

    Removed nav pane block controls

--- src/modules/nav-pane/components/Block.js
+++ src/modules/nav-pane/components/Block.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import traxExt from '../../trax-ext'
-import ui from '../../ui'
-import BlockControls from '../containers/BlockControls'
-
-// <TODO>
 
 const NavPaneBlock = (props) => (
   <div className="nav-pane-block">
-    <div className="block-position-label">{props.i + 1}</div>
     <traxExt.components.Block {...props} />
-    <ui.components.ToggleMenu>
-      <BlockControls id={props.block.id} />
-    </ui.components.ToggleMenu>
+    <div className="block-position-label">{props.i + 1}</div>
   </div>
 )
 
