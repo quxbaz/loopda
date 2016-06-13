9756c4c6225005765031459cff51320ba82df7f1
David Yeung <quxbaz@gmail.com>
2016-06-13 15:56:56 -0400

    Namespaced util method

--- src/modules/blocks/containers/Channel.js
+++ src/modules/blocks/containers/Channel.js
@@ -1,26 +1,28 @@
 import {connect} from 'react-redux'
 import {channels} from 'trax'
 import traxExt from '../../trax-ext'
-import {getBeatClicked} from '../util'
+import util from '../util'
 
 const mapDispatchToProps = (dispatch, {channel}) => ({
+
   onMouseDown(event, el) {
-    const beat = getBeatClicked(event, el)
+    const beat = util.getBeatClicked(event, el)
     dispatch(channels.actions.toggleBlipAt(channel.id, beat))
     return dispatch((dispatch, getState) => {
       return channels.selectors.getBlipAt(channel.id, beat)(getState()).mute
     })
   },
+
   onMouseMove(event, el, blipWasMuted) {
-    const beat = getBeatClicked(event, el)
+    const beat = util.getBeatClicked(event, el)
     if (blipWasMuted) {
       dispatch(channels.actions.muteBlipAt(channel.id, beat))
     } else {
       dispatch(channels.actions.unmuteBlipAt(channel.id, beat))
     }
   },
-})
 
+})
 
 export default connect(
   null,
