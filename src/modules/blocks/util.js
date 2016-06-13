00cf08ad08aecf34ef519050be09c15a6e09fb33
David Yeung <quxbaz@gmail.com>
2016-06-13 15:49:53 -0400

    Removed util method

--- src/modules/blocks/util.js
+++ src/modules/blocks/util.js
@@ -1,14 +1,18 @@
 const BEATS = 16
 
-export const getBeatClicked = (event, el) => {
-  /*
-    Detects which beat position was clicked by looking at the position
-    of the mouse click compared to the dimensions of the channel
-    element. This exists as an optimization that bypasses the need to
-    render individual click-watching blip elements.
-  */
-  const {left, right} = el.getBoundingClientRect()
-  const channelWidth = right - left
-  const blipWidth = channelWidth / BEATS
-  return Math.floor((event.clientX - left) / blipWidth)
+export default {
+
+  getBeatClicked(event, el) {
+    /*
+      Detects which beat position was clicked by looking at the position
+      of the mouse click compared to the dimensions of the channel
+      element. This exists as an optimization that bypasses the need to
+      render individual click-watching blip elements.
+    */
+    const {left, right} = el.getBoundingClientRect()
+    const channelWidth = right - left
+    const blipWidth = channelWidth / BEATS
+    return Math.floor((event.clientX - left) / blipWidth)
+  }
+
 }
