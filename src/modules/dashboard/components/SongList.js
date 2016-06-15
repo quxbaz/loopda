7cc7219eab70244052249c75d81392134fc4b602
David Yeung <quxbaz@gmail.com>
2016-06-15 13:48:17 -0400

    Rendering empty song list

--- src/modules/dashboard/components/SongList.js
+++ src/modules/dashboard/components/SongList.js
@@ -1,11 +1,16 @@
 import React, {PropTypes} from 'react'
 import SongItem from '../providers/SongItem'
 
+const EmptyList = () => (
+  React.DOM.em({}, "You don't have any songs yet")
+)
+
 const SongList = ({ids}) => (
   <div className="song-list">
     {ids.map((id) => (
       <SongItem key={id} id={id} />
     ))}
+    {ids.length > 0 ? null : <EmptyList />}
   </div>
 )
 
